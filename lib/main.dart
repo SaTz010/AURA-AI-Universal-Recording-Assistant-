45d9a16eaf482a29eea98fae2e7712d0c2556857
melt-sanskar <tsanskar22@tbc.edu.np>
2026-03-22 11:09:08 +0545

    Feat:intalled and configured firebase inside the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,14 @@ import 'screens/settings_screen.dart';
 import 'screens/summary_screen.dart';
 import 'theme/aura_theme.dart';
 import 'theme/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import firebase_core
 import just_audio
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
 }
